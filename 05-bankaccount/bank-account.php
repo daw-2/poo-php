4f57e2c6627ef8f1e918aa1482df7c7188857dab
Matthieu Mota <matthieumota@gmail.com>
2019-08-06 15:09:41 +0200

    Bank account tp

---
+++ 05-bankaccount/bank-account.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Nous allons créer un système de gestion de compte en banque en POO.
+ * 
+ * Nous aurons d'abord le compte en banque classique représentée par la classe BankAccount.
+ * Il possédera un identifiant (int), un propriétaire (string) et un montant (float).
+ * On devra définir l'identifiant et le propriétaire dans le constructeur. Le montant pourra être défini dans le constructeur de manière optionnelle.
+ */
+
+$bankAccount01 = new BankAccount(123456, 'Matthieu'); // Matthieu a 0 sur son compte
+echo $bankAccount01->getBalance(); // Renvoie 0
+$bankAccount01->depositMoney(1000); // Matthieu a 1000 sur son compte
+echo $bankAccount01->getBalance(); // Renvoie 1000
+$bankAccount01->withdrawMoney(1000); // Matthieu a 0 sur son compte
+
+// On renvoie une erreur si le montant du compte tombe en dessous de 0
+$bankAccount01->withdrawMoney(1000);
+$bankAccount01->depositMoney(-2000);
