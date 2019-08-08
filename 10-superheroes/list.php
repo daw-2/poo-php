4e08b9301aa0c3847cd6551ff63eefed686aaa3c
Matthieu Mota <matthieumota@gmail.com>
2019-08-08 14:49:06 +0200

    TP list heroes

---
+++ 10-superheroes/list.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Reprendre le header (de !DOCTYPE jusque <body>) de create.php et l'ajouter dans un fichier partials/header.php
+ * Reprendre le footer (de la première balise <script> à </html>) de create.php et l'ajouter dans un fichier partials/footer.php
+ * 
+ * Dans le fichier list.php, inclure le header et inclure le footer.
+ * Entre le header et le footer, on créera un tableau HTML avec Bootstrap.
+ * Dans ce tableau, on affichera la liste des super héros présents dans la base de données.
+ * 
+ * Une fois le fichier list.php terminé, on ajoutera une navbar dans le partials/header.php. La navbar permettra de naviguer entre la page list.php (Les héros) et la page create.php (Créer un héros). Il faudra bien inclure le header et le footer dans create.php
+ */
