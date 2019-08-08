bb28430c0fe5d4c0b5af694b402d6f1490e3bf2a
Matthieu Mota <matthieumota@gmail.com>
2019-08-08 11:36:31 +0200

    Prepare create superheroes

---
+++ 10-superheroes/create.php
@@ -0,0 +1,9 @@
+<!--
+    1/ Intégrer une nouvelle page HTML (DOCTYPE...) avec Bootstrap
+    2/ Ajouter un formulaire permettant de créer un super héros
+       Le formulaire contient les champs : name (input), power (input), identity (input), universe (select)
+    3/ Ajouter le traitement du formulaire :
+       - Quand le formulaire est soumis, on récupére les données dans $_POST
+       - A partir des données, on va créer une instance de SuperHeroe et hydrater celle-ci.
+       - Reprendre la requête SQL pour créer un super héros et on l'adapte pour pouvoir ajouter l'instance créée précédement.
+-->
